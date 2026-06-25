acf55c8fe0b19852d2cce73847d1fb091d9cdae0
Christian Rackerseder <git@echooff.de>
2026-06-25 07:49:32 +0200

    Add lint and formatting configuration

---
+++ eslint.config.js
@@ -0,0 +1,72 @@
+import { baseConfig } from '@enormora/eslint-config-base';
+import { nodeConfig } from '@enormora/eslint-config-node';
+import { mochaConfig } from '@enormora/eslint-config-mocha';
+import { typescriptConfig } from '@enormora/eslint-config-typescript';
+
+// cspell:ignore sonarjs
+
+export default [
+    {
+        ignores: ['coverage/**', 'target/**', 'mocha.config.json']
+    },
+    {
+        ...baseConfig,
+        rules: {
+            ...baseConfig.rules,
+            'import/no-unused-modules': [
+                'error',
+                {
+                    unusedExports: true,
+                    suppressMissingFileEnumeratorAPIWarning: true
+                }
+            ]
+        }
+    },
+    nodeConfig,
+    {
+        ...typescriptConfig,
+        files: ['**/*.ts'],
+        rules: {
+            ...typescriptConfig.rules,
+            'import/extensions': [
+                'error',
+                'always',
+                {
+                    ignorePackages: true
+                }
+            ]
+        }
+    },
+    {
+        ...mochaConfig,
+        files: ['**/*.test.ts'],
+        languageOptions: {
+            globals: {
+                test: 'readonly'
+            }
+        },
+        rules: {
+            ...mochaConfig.rules,
+            'sonarjs/no-empty-group': 'off',
+            complexity: 'off',
+            'mocha/no-global-tests': 'off',
+            'mocha/no-mocha-arrows': 'off'
+        }
+    },
+    {
+        rules: {
+            '@typescript-eslint/method-signature-style': 'off',
+            '@typescript-eslint/no-unsafe-type-assertion': 'off',
+            '@stylistic/operator-linebreak': 'off',
+            'import/no-named-as-default': 'off',
+            'import/no-named-as-default-member': 'off',
+            'max-lines-per-function': ['error', { max: 50, skipBlankLines: true, skipComments: true }]
+        }
+    },
+    {
+        files: ['eslint.config.js', 'mocha.config.json', 'prettier.config.js'],
+        rules: {
+            'import/no-default-export': 'off'
+        }
+    }
+];
---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+const prettierConfig = {
+    printWidth: 120,
+    tabWidth: 4,
+    useTabs: false,
+    semi: true,
+    singleQuote: true,
+    trailingComma: 'none',
+    bracketSpacing: true,
+    arrowParens: 'always'
+};
+
+export default prettierConfig;
